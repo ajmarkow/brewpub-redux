fa02409e2dd935bf57a0a17605313f95ba74baeb
AJ Markow <alexanderjmarkow@gmail.com>
2020-12-04 14:17:04 -0800

    Add test for keglist reducer. Add keglist reducer.

--- src/components/__tests__/reducers/KegListReducer.test.js
+++ src/components/__tests__/reducers/KegListReducer.test.js
@@ -6,5 +6,37 @@ describe('KegListReducer', () =>{
     })
 });
 
-
+describe('KegListReducer', () =>{
+  let action;
+  const kegData = {
+    name:'DOUBLE IPA',
+    brand:'DESCHUTES',
+    price: 6,
+    abv: 8,
+    fluidOunces:124,
+    id: 1
+  }
+  test('The reducer for list of kegs sholud return default if we tell it to do nothing.', () => {
+      const {name,brand,price,abv,fluidOunces,id}= kegData;
+      action = {
+        type: 'ADD_KEG',
+        name:'DOUBLE IPA',
+        brand:'DESCHUTES',
+        price:6,
+        abv:abv,
+        fluidOunces:124,
+        id: id
+      };
+      expect(KegListReducer({},action)).toEqual({
+        [id]:{
+          name:name,
+          brand:brand,
+          price:price,
+          abv:abv,
+          fluidOunces:fluidOunces,
+          id:1
+        }
+      })
+    })
+});
       
--- src/reducers/KegListReducer.js
+++ src/reducers/KegListReducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-
+  const {name,brand,price,abv,fluidOunces, id}= action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          name:name,
+          brand:brand,
+          price:price,
+          abv:abv,
+          fluidOunces:fluidOunces,
+          id:id
+        }
+      });
+      default:
+        return state;
+  }
 };
